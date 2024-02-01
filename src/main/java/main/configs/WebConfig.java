de0ec229981751c294c3ae0f62a6f6e5b3e71e3c
Roman Surkov <s.rb@list.ru>
2024-02-01 22:57:22 +0200

    Added webconfig for cors

---
+++ src/main/java/main/configs/WebConfig.java
@@ -0,0 +1,26 @@
+package main.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final String[] ALLOWED_METHODS = {"GET", "POST", "PUT", "DELETE"};
+    private static final String[] ALLOWED_ORIGINS = {
+            "http://localhost:3000",
+            "http://surkoff.com",
+            "http://blog.surkoff.com",
+            "https://surkoff.com"
+    };
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedMethods(ALLOWED_METHODS)
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
+    }
+}
